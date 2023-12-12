a88e6ad2781cf49d3f50e45e95aa187ca25da46e
Manith Fernando <manithf4@gmail.com>
2023-12-12 11:44:47 +1000

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,7 +3,7 @@ weight = int(input("Weight: "))
 unit = input("(K)g or (L)bs: ")
 if unit.upper() == "K":
     converted = weight / 0.45
-    print("Weight in Lbs: " + str(converted))
+    print(f"Weight in Lbs: {str(converted)}")
 else:
     converted = weight * 0.45
-    print("Weight in Kgs: " + str(converted))
+    print(f"Weight in Kgs: {str(converted)}")
